73c369bed9b723763c072cbb0843fe86dccf9d17
Jpduenas17 <danpao45@gmail.com>
2022-08-18 20:06:24 -0500

    Prometeus

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import requests
 
+
 from typing import Union
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+from prometheus_fastapi_instrumentator import Instrumentator 
+
 
 app = FastAPI()
 
@@ -27,3 +30,5 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+Instrumentator().instrument(app).expose(app)
