16bcdf014fc1d6c33e278c7b2491dbf6551127ba
Jpduenas17 <danpao45@gmail.com>
2022-08-21 13:03:42 -0500

    Error 204

--- main_test.py
+++ main_test.py
@@ -6,5 +6,9 @@ client = TestClient(app)
 
 
 def test_index_route():
-    response = client.get('/infoUsers/Pasquale_West92')
+    response = client.get('/infoUsers/Celia_Harber9')
     assert response.status_code == 200
+
+def test_index_204():
+    response = client.get('/infoUsers/Pasquale_West92')
+    assert response.status_code == 204
